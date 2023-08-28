ad3363e5a9b29a3c493ecadfc6afe76c974a331c
NhatChu <chunhat321123@gmail.com>
2023-08-28 15:54:32 +0700

    feat: cart status

--- 03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Product } from 'src/app/common/product';
+import { CartItem } from 'src/app/common/cart-item';
 import { ProductService } from 'src/app/services/product.service';
+import { CartService } from 'src/app/services/cart.service';
 
 @Component({
   selector: 'app-product-list',
@@ -22,7 +24,9 @@ export class ProductListComponent implements OnInit {
   
   previousKeyword: string = "";
   
-  constructor(private productService: ProductService, private route: ActivatedRoute) { }
+  constructor(private productService: ProductService, 
+              private cartService: CartService,
+              private route: ActivatedRoute) { }
 
   ngOnInit(): void {
     this.route.paramMap.subscribe(() =>{
@@ -116,5 +120,14 @@ export class ProductListComponent implements OnInit {
                                               this.thePageSize,
                                               this.currentCategoryId)
                                               .subscribe(this.processResult());
+  }
+
+  addToCart(theProduct : Product)
+  {
+    console.log(`Adding to cart: ${theProduct.name}, ${theProduct.unitPrice}`);
+
+    const theCartItem = new CartItem(theProduct);
+    this.cartService.addToCart(theCartItem);
+    
   }
 }
