d6167339952541e9b0e6633defc333cca6591956
NhatChu <chunhat321123@gmail.com>
2023-09-26 15:22:16 +0700

    feat: email receipt

--- 03-frontend/angular-ecommerce/src/app/config/my-app-config.ts
+++
@@ -1,8 +0,0 @@
-export default  {
-    oidc:{
-        clientId: '0oab99toj5fAOHD7D5d7',
-        issuer: 'https://dev-31228919.okta.com/oauth2/default',
-        redirectUri: 'https://localhost:4200/login/callback',
-        scopes: ['openid', 'profile', 'email']
-    }
-}
